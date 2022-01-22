ec61d1e2114fef2b67b5098fbe35a37686c28b30
leesangsuk <skysangsuk@gmail.com>
2022-01-22 15:39:47 +0900

    Config update

---
+++ crayondiary-dev/server/config/config.js
@@ -0,0 +1,23 @@
+{
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "crayondiary_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "",
+    "database": "crayondiary_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "",
+    "database": "crayondiary_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
