7bc4128000cb241161b031366fcc9dc940ffdb3f
Jaekomplett <wanzekim@gmail.com>
2022-01-22 18:21:54 +0900

    First commit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,138 @@
 import React from "react";
+import styled from "styled-components";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import { faGithub } from "@fortawesome/free-brands-svg-FaIcons"
+
+// footer는 높이를 지정하지 않는다.
+// 푸터 스타일
+// 푸터 컨테이너
+// 푸터 컨테이너 내부 컴포넌트
+// 제목, 본문 | 깃헙 링크 | 멤버 깃헙 링크
+
+const StyledFooter = styled.footer`
+  min-height: 100%;
+  position: fixed; // 스크롤을 움직여도 위치가 고정된다.
+  padding: 4rem 2rem;
+  border-top: 1px solid var(--color-lightgray);
+  font-family: Interop-Regular;
+  /* width: 100%; */
+  /* bottom: 0; */
+`;
+
+const Container = styled.div`
+  display: flex;
+  margin-bottom: 3rem;
+  gap: 3rem;
+`;
+
+const DescWrapper = styled.div`
+  flex: 2;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  list-style: none;
+
+`;
+
+const Heading = styled.h1`
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  font-family: Interop-Medium;
+  color: var(--color-black);
+`;
+
+const Paragraph = styled.p`
+  word-break: keep-all;
+  line-height: var(--lineHeight-relaxed);
+`;
+
+const Items = styled.ul``;
+
+const Item = styled.li`
+  :not(:last-child) {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const Link = styled.a`
+  text-decoration: underline;
+  /* svg {
+    vertical-align: text-bottom;
+    font-size: 1.25rem;
+    margin-right: 0.5rem;
+  } */
+`;
+
+const Copyright = styled.span``;
 
 const Footer = () => {
-  return <div>항상 페이지 하단에 위치</div>;
+  return (
+    <StyledFooter>
+      <Container>
+        <DescWrapper>
+          <Heading>Crayon Diary</Heading>
+          <Paragraph>
+            Crayon Diary는 어렸을 때 그렸던 그림 일기처럼 일상을 그림으로
+            기록하는 웹 서비스입니다. 일기장과 크레파스를 직접 구매하는 대신,
+            웹을 통해 간편하게 그림 일기 감성을 다시 한 번 느낄 수 있는 가치를
+            제공하기 위해 기획되었습니다.
+          </Paragraph>
+        </DescWrapper>
+        <Wrapper>
+          <Heading>Project</Heading>
+          <Items>
+            <Item>
+              <Link
+                href="https://github.com/codestates/crayondiary"
+                target="_blank"
+              >
+                Github
+              </Link>
+            </Item>
+            <Item>
+              <Link
+                href="https://github.com/codestates/crayondiary/wiki"
+                target="_blank"
+              >
+                Wiki
+              </Link>
+            </Item>
+          </Items>
+        </Wrapper>
+        <Wrapper>
+          <Heading>Team Members</Heading>
+          <Items>
+            <Item>
+              <Link href="https://github.com/Jaekomplett" target="_blank">
+                김제완
+              </Link>
+            </Item>
+            <Item>
+              <Link href="https://github.com/alsrlqor" target="_blank">
+                백민기
+              </Link>
+            </Item>
+            <Item>
+              <Link href="https://github.com/leesangsuk-cloud" target="_blank">
+                이상석
+              </Link>
+            </Item>
+            <Item>
+              <Link href="https://github.com/sunwoong3" target="_blank">
+                정선우
+              </Link>
+            </Item>
+          </Items>
+        </Wrapper>
+      </Container>
+
+      <Copyright>© 2021 Crayondiary. All rights reserved.</Copyright>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
+
+
+
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,7 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Logo from "../images/titlelogo.png"
+
+const TitleBoard = styled.main`
+  width: 100%;
+  height: 31.25rem;
+  background: #FFF1AD;
+`;
+
+const TitleLogo = styled.img`
+  display: flex;
+  padding-top: 8.5rem;
+  width: 630px;
+  height: 239px;
+  margin: auto;
+`
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  /* height: 100vh; */
+  flex-direction: column;
+  
+  /* 
+  row  | | |
+
+  column |
+         |
+         |
+  */
+`;
+
+
+const FeedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* gap: 1rem; */
+  `
+
+// padding: 9.375 10.625 9.375 10.625rem;
+// padding: 150 170 150 170;
+
+const TapMenu = styled.ul`
+  color: rgba(73, 73, 73, 0.5);
+  display: flex; // flex-box 형태로 개별 단위로 나뉜다.
+  margin: 0 18.75rem; // 
+  font-size: 1.25rem;
+  font-weight: bold;
+  list-style: none; // ul 포인트 삭제
+  gap: 3rem;
+
+  .submenu {
+    width: 100%;
+    margin: 2rem;
+    cursor: pointer;
+  }
+
+  .selected {
+    color: black;
+    transition: 0.2s;
+  }
+`;
+
+const FeedPhoto = styled.div` 
+  display: flex;
+  width: 26.25rem;
+  height: 28.125rem;
+  background-color: #white;
+  box-shadow: 3px 3px 10px 0px rgba(0, 0, 0, 0.17);
+  border-radius: 1.875rem; 
+  margin: 0 18.75rem; 
+  cursor: position;
+  
+`
+
+
 
 const MainPage = () => {
-  return <div>메인 크레용 다이어리 페이지</div>;
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const menuArr = [{ name: "최신순" }, { name: "인기순" }, { name: "팔로잉" }];
+
+  const selectMenuHandler = (idx) => {
+    setCurrentTab(idx);
+  };
+
+  return (
+    <>
+      <Container>
+        <TitleBoard>
+          <TitleLogo src={Logo}/>
+        </TitleBoard>
+        <FeedContainer>
+
+          <TapMenu>
+            {menuArr.map((el, idx) => {
+              return (
+                <li
+                  key={idx}
+                  className={
+                    currentTab === idx ? "submenu selected" : "submenu"
+                  }
+                  onClick={() => selectMenuHandler(idx)}
+                >
+                  {el.name}
+                </li>
+              );
+            })}
+          </TapMenu>
+          <FeedPhoto />
+          <FeedPhoto />
+          <FeedPhoto />
+
+        </FeedContainer>
+      </Container>
+    </>
+  );
 };
 
 export default MainPage;
