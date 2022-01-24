d501a9a35f055291770ef42ea6ddb1dc179f954c
Jaekomplett <wanzekim@gmail.com>
2022-01-24 21:52:03 +0900

    Footer Update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../images/footerlogo.png";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { faGithub } from "@fortawesome/free-brands-svg-FaIcons"
 
 // footer는 높이를 지정하지 않는다.
 // 푸터 스타일
@@ -9,69 +9,95 @@ import styled from "styled-components";
 // 푸터 컨테이너 내부 컴포넌트
 // 제목, 본문 | 깃헙 링크 | 멤버 깃헙 링크
 
-const StyledFooter = styled.footer`
-  min-height: 100%;
-  position: fixed; // 스크롤을 움직여도 위치가 고정된다.
-  padding: 4rem 2rem;
-  border-top: 1px solid var(--color-lightgray);
-  font-family: Interop-Regular;
-  /* width: 100%; */
-  /* bottom: 0; */
+// flex-direction
+// row | | |
+// column ㅡ
+//        ㅡ
+//        ㅡ
+// Footer 설정 point 
+// bottom: 0이 기본이 되어야 함
+
+const FooterWrapper = styled.footer`
+  font-family: 'Noto Sans KR';
+  color: #424242;
+
+  /* min-height: 100vh; */
+  /* position: fixed; */
+  bottom: 0; 
+  // 하씨... 이놈이 범인이었다.
+  // 해당 태그 위치를 하단 기준으로 어느 높이에 위치시킬건지 지정.
+  left: 0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  /* position: fixed; // 스크롤을 움직여도 위치가 고정된다. */
+  padding-top: 3.5rem;
+  // padding: 상 좌우 하
+  border-top: solid 0.2rem #ecf0f1;
 `;
 
 const Container = styled.div`
   display: flex;
-  margin-bottom: 3rem;
-  gap: 3rem;
+  margin: 0 0 3rem 3rem;
+  /* gap: 3rem; */
 `;
 
 const DescWrapper = styled.div`
   flex: 2;
+  padding-right: 17rem;
 `;
 
 const Wrapper = styled.div`
   flex: 1;
   list-style: none;
-
+  /* padding-top: 20px; */
 `;
 
+const FooterLogo = styled.img`
+  width: 300px;
+  height: 47px;
+`
+
+
 const Heading = styled.h1`
-  font-size: 1rem;
+  font-size: 1.1rem;
   margin-bottom: 1rem;
-  font-family: Interop-Medium;
-  color: var(--color-black);
 `;
 
 const Paragraph = styled.p`
+  font-family: 'Roboto';
   word-break: keep-all;
   line-height: var(--lineHeight-relaxed);
+  font-size: 1rem;
 `;
-
-const Items = styled.ul``;
 
 const Item = styled.li`
-  :not(:last-child) {
-    margin-bottom: 0.5rem;
+  margin-bottom: 0.5rem;
+`;
+
+
+const Href = styled.a`
+  color: #424242;
+  :hover {
+    color: #ff7a1b;
   }
+  text-decoration: none;
 `;
 
-const Link = styled.a`
-  text-decoration: underline;
-  /* svg {
-    vertical-align: text-bottom;
-    font-size: 1.25rem;
-    margin-right: 0.5rem;
-  } */
+const Copyright = styled.span`
+  margin-left: 3rem;
+  margin: 0 0 4rem 3rem;
 `;
 
-const Copyright = styled.span``;
+
 
 const Footer = () => {
   return (
-    <StyledFooter>
+    <FooterWrapper>
       <Container>
         <DescWrapper>
-          <Heading>Crayon Diary</Heading>
+          <FooterLogo src={Logo}/>
           <Paragraph>
             Crayon Diary는 어렸을 때 그렸던 그림 일기처럼 일상을 그림으로
             기록하는 웹 서비스입니다. 일기장과 크레파스를 직접 구매하는 대신,
@@ -80,55 +106,56 @@ const Footer = () => {
           </Paragraph>
         </DescWrapper>
         <Wrapper>
-          <Heading>Project</Heading>
-          <Items>
+          
+          <Heading>About Us</Heading>
             <Item>
-              <Link
+              <Href
                 href="https://github.com/codestates/crayondiary"
                 target="_blank"
               >
+                <></>
                 Github
-              </Link>
+              </Href>
             </Item>
             <Item>
-              <Link
+              <Href
                 href="https://github.com/codestates/crayondiary/wiki"
                 target="_blank"
               >
                 Wiki
-              </Link>
+              </Href>
             </Item>
-          </Items>
         </Wrapper>
-        <Wrapper>
-          <Heading>Team Members</Heading>
-          <Items>
-            <Item>
-              <Link href="https://github.com/Jaekomplett" target="_blank">
-                김제완
-              </Link>
-            </Item>
-            <Item>
-              <Link href="https://github.com/alsrlqor" target="_blank">
-                백민기
-              </Link>
-            </Item>
-            <Item>
-              <Link href="https://github.com/leesangsuk-cloud" target="_blank">
-                이상석
-              </Link>
-            </Item>
-            <Item>
-              <Link href="https://github.com/sunwoong3" target="_blank">
-                정선우
-              </Link>
-            </Item>
-          </Items>
-        </Wrapper>
-      </Container>
 
-      <Copyright>© 2021 Crayondiary. All rights reserved.</Copyright>
-    </StyledFooter>
+        <Wrapper>
+          <Heading>Contact</Heading>
+            <Item>
+              <Href href="https://github.com/Jaekomplett" target="_blank">
+                {/* <img src={GithubLogo}></img> */}
+                김제완
+              </Href>
+            </Item>
+            <Item>
+              <Href href="https://github.com/alsrlqor" target="_blank">
+                백민기
+              </Href>
+            </Item>
+            <Item>
+              <Href href="https://github.com/leesangsuk-cloud" target="_blank">
+                이상석
+              </Href>
+            </Item>
+            <Item>
+              <Href href="https://github.com/sunwoong3" target="_blank">
+                정선우
+              </Href>
+            </Item>
+        </Wrapper>
+        
+      </Container>
+      <Copyright>© 2022 Crayondiary. All rights reserved.</Copyright>
+
+    </FooterWrapper>
   );
 };
 
