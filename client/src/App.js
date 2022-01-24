ccd1012e0fe3d1ad5daf32a72f15fa7344e69180
서누 <nung3@jeongseon-uui-MacBookAir.local>
2022-01-25 06:47:13 +0900

    App.js Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import EditUserInfo from "./pages/EditUserInfo";
 import MainPage from "./pages/MainPage";
-import PostPage from "./pages/PostPage";
-import ViewPostPage from "./pages/ViewPostPage";
-import WritePage from "./pages/WritePage";
+import CreatePost from "./pages/CreatePost";
+import ViewPost from "./pages/ViewPost";
+import UserFeed from "./pages/UserFeed";
 import Nav from "./components/Nav";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import FollowingPage from "./pages/FollowingPage";
-import FollowerModal from "./components/FollowerModal";
 import Footer from "./components/Footer";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
@@ -17,14 +14,11 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/sign/login" element={<LoginPage />} />
         <Route path="/user/:userId" element={<EditUserInfo />} />
-        <Route path="/feeds" element={<PostPage />} />
-        <Route path="/post/:postId" element={<ViewPostPage />} />
-        <Route path="/post" element={<WritePage />} />
-        <Route path="/sign/register" element={<SignupPage />} />
-        <Route path="/follow/following/:userId" element={<FollowingPage />} />
-        <Route path="/follow/follower/:userId" element={<FollowerModal />} />
+        <Route path="/feeds" element={<UserFeed />} />
+        <Route path="/post/:postId" element={<ViewPost />} />
+        <Route path="/post" element={<CreatePost />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
       <Footer />
     </>
