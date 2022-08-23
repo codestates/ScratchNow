c8d67744c48d55721a1288f018c2272e40dfb164
alsrlqor <skyup9393@gmail.com>
2022-08-23 23:54:03 +0900

    Add migration files

---
+++ server/migrations/createTables.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path'
+import { exec } from 'child_process';
+import * as util from 'util';
+
+const asyncExec = util.promisify(exec);
+
+console.log(`
+  --------------------------------------
+  +++ScratchNow DB Migration Start+++
+  --------------------------------------
+`);
+
+let migrationAllTable = async () => {
+    let migrationFiles : string[] = [];
+
+    fs.readdir(path.join(__dirname,"/","tables"), async (err,files) => {
+        if (err) console.log("err: ", err);
+        if (files) {
+            files.forEach(el => {
+                migrationFiles.push(el);
+            })
+
+            for(let el of migrationFiles) {
+                console.log("Migration File Name: ", el);
+
+                const { stdout, stderr } = await asyncExec(`./node_modules/.bin/ts-node "${__dirname}/tables/${el}"`)
+                if(stdout) console.log(stdout);
+                if(stderr) console.error("Std Err: ", stderr);
+            }
+        }
+    })
+};
+
+migrationAllTable()
