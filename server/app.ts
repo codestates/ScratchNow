e97f973656d3668293dd66062af412bb4c7b3ec5
alsrlqor <skyup9393@gmail.com>
2022-08-23 23:58:15 +0900

    Rewrite env selection code

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import cors from 'cors';
+import routes from "./routes";
 import * as dotenv from "dotenv";
 import * as path from 'path';
 import * as ip from 'ip';
@@ -11,32 +12,19 @@ import { sequelize } from "./models";
 dotenv.config({
     path: path.resolve(
         process.env.NODE_ENV === 'production'
-            ? '.prod.env' : '.dev.env'
+            ? '.prod.env' : process.env.NODE_ENV === 'test'
+            ? '.test.env' : '.dev.env'
     )
 });
 
 const app = express();
-
 const PROTOCOL = 'http';
 const myIp = ip.address();
 const PORT = process.env.SERVER_PORT;
-const API_VER = 'v1';
-
-const SignRouter = require('./routes/SignRouter');
-const UserinfoRouter = require('./routes/UserinfoRouter');
-const PostsRouter = require('./routes/PostsRouter');
-const FeedsRouter = require('./routes/FeedsRouter');
-const CommentsRouter = require('./routes/CommentsRouter');
-const LikesRouter = require('./routes/LikesRouter');
 
 app.use(express.json());
 app.use(cors());
-app.use(`/api/${API_VER}/sign`, SignRouter);
-app.use(`/api/${API_VER}/userinfo`, UserinfoRouter);
-app.use(`/api/${API_VER}/post`, PostsRouter);
-app.use(`/api/${API_VER}/feed`, FeedsRouter);
-app.use(`/api/${API_VER}/comment`, CommentsRouter);
-app.use(`/api/${API_VER}/like`, LikesRouter);
+app.use(routes);
 
 const swaggerSpec = YAML.load(path.join(__dirname, './swagger/swagger.yaml'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
