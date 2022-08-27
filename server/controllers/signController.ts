2d84b45b70c5e5316cc7cdaf27100be6d0af4518
alsrlqor <skyup9393@gmail.com>
2022-08-27 23:45:36 +0900

    Import and test bcrypt module

--- server/controllers/signController.ts
+++ server/controllers/signController.ts
@@ -1,8 +1,26 @@
 import express, {Request, Response} from "express";
+import {Users} from '../models/users';
+import { sign, verify} from 'crypto';
+import * as bcrypt from 'bcrypt';
 
 const signHandler = {
     signup: async (req: Request, res: Response) => {
+        const { email, password, nickname } = req.body;
+        const emailValidity = await Users.findOne({ where:{ email } });
+        const nicknameValidity = await Users.findOne({ where:{ nickname } });
 
+        if (emailValidity) {
+            res.json({ message: 'Email Exists' });
+        } else if (nicknameValidity) {
+            res.json({ message: 'Nickname Exists' });
+        } else {
+            const salt = await bcrypt.genSalt(4);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            await Users.create({email, password: hashedPassword, nickname}).then((data) => {
+                res.status(201).json({message: 'registered'});
+            });
+        };
     },
 
     login: async (req: Request, res: Response) => {
