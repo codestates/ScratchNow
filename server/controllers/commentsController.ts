9cd28676f798fc489b6be6e6431f4eaa7ea50f21
alsrlqor <skyup9393@gmail.com>
2022-09-05 23:59:14 +0900

    #74 Add Comments feature - 기본 댓글 작성 및 수 API 생성 - Handler 명칭 변경

--- server/controllers/commentsController.ts
+++ server/controllers/commentsController.ts
@@ -1,23 +1,41 @@
-import express, {Request, Response} from "express";
-import {Comments} from "../models/comments";
+import express, { Request, Response } from 'express';
+import { Comments } from '../models/comments';
+import { tokenAuthentication } from './tokenFunctions';
+import { Posts } from '../models/posts';
 
-const commentsHandler = {
-    createComment: async (req: Request, res: Response) => {
-        const { user_id, post_id, text, anonymity_yn } = req.body;
-        // await Comments.create({user_id, post_id, anonymity_yn, text})
-    },
+const commentsController = {
+  createComment: async (req: Request, res: Response) => {
+    const { user_id, post_id, text, anonymity_yn } = req.body;
 
-    modifyComment: async (req: Request, res: Response) => {
+    await Comments.create({ user_id, post_id, anonymity_yn, text }).then(
+      (data) => {
+        res
+          .status(201)
+          .json({ postData: data, message: `Created the comment` });
+      },
+    );
+  },
 
-    },
+  modifyComment: async (req: Request, res: Response) => {
+    const tokenValidity = tokenAuthentication(req);
+    const { id, text, anonymity_yn } = req.body;
 
-    deleteComment: async (req: Request, res: Response) => {
-
-    },
-
-    getComments: async (req: Request, res: Response) => {
-
+    if (!tokenValidity) {
+      res.status(404).json({ message: 'Invalid Token' });
+    } else {
+      try {
+        await Posts.update({ text }, { where: { id } }).then(() => {
+          res.status(200).json({ message: `Modified the comment` });
+        });
+      } catch (err) {
+        res.status(404).json({ message: 'Invalid comment id' });
+      }
     }
+  },
+
+  deleteComment: async (req: Request, res: Response) => {},
+
+  getComments: async (req: Request, res: Response) => {},
 };
 
-export default commentsHandler;
+export default commentsController;
