4c8e7e04a06246fbeabe9ad98545b2b0a7d9257e
alsrlqor <skyup9393@gmail.com>
2022-09-06 22:48:58 +0900

    #74 Comment features - 댓글 api(생성, 수정, 삭제, 조회) 추가 및 보완

--- server/controllers/commentsController.ts
+++ server/controllers/commentsController.ts
@@ -1,41 +1,79 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { Comments } from '../models/comments';
 import { tokenAuthentication } from './tokenFunctions';
-import { Posts } from '../models/posts';
 
 const commentsController = {
   createComment: async (req: Request, res: Response) => {
-    const { user_id, post_id, text, anonymity_yn } = req.body;
-
-    await Comments.create({ user_id, post_id, anonymity_yn, text }).then(
-      (data) => {
-        res
-          .status(201)
-          .json({ postData: data, message: `Created the comment` });
-      },
-    );
-  },
-
-  modifyComment: async (req: Request, res: Response) => {
     const tokenValidity = tokenAuthentication(req);
-    const { id, text, anonymity_yn } = req.body;
+    const { user_id, post_id, text, anonymity_yn } = req.body;
 
     if (!tokenValidity) {
       res.status(404).json({ message: 'Invalid Token' });
     } else {
       try {
-        await Posts.update({ text }, { where: { id } }).then(() => {
-          res.status(200).json({ message: `Modified the comment` });
-        });
+        await Comments.create({ user_id, post_id, anonymity_yn, text }).then(
+          (data) => {
+            res
+              .status(201)
+              .json({ postData: data, message: `Created the comment` });
+          },
+        );
       } catch (err) {
-        res.status(404).json({ message: 'Invalid comment id' });
+        res.status(500).json({ message: 'Failed to create comment' });
       }
     }
   },
 
-  deleteComment: async (req: Request, res: Response) => {},
+  modifyComment: async (req: Request, res: Response) => {
+    const tokenValidity = tokenAuthentication(req);
+    const { comment_id, text, anonymity_yn } = req.body;
 
-  getComments: async (req: Request, res: Response) => {},
+    if (!tokenValidity) {
+      res.status(404).json({ message: 'Invalid Token' });
+    } else {
+      try {
+        await Comments.update(
+          { text, anonymity_yn },
+          { where: { id: comment_id } },
+        ).then(() => {
+          res.status(200).json({ message: `Modified the comment` });
+        });
+      } catch (err) {
+        res.status(500).json({ message: 'Failed to modify comment' });
+      }
+    }
+  },
+
+  deleteComment: async (req: Request, res: Response) => {
+    const tokenValidity = tokenAuthentication(req);
+    const { comment_id } = req.body;
+
+    if (!tokenValidity) {
+      res.status(404).json({ message: 'Invalid Token' });
+    } else {
+      try {
+        await Comments.destroy({ where: { id: comment_id } }).then(() => {
+          res.status(200).json({ message: `Soft deleted the comment` });
+        });
+      } catch (err) {
+        res.status(500).json({ message: 'Failed to delete comment' });
+      }
+    }
+  },
+
+  getComments: async (req: Request, res: Response) => {
+    const { post_id } = req.body;
+
+    try {
+      await Comments.findAll({ where: { post_id } }).then((data) => {
+        res
+          .status(200)
+          .json({ data: data, message: `Comments of post ${post_id}` });
+      });
+    } catch (err) {
+      res.status(500).json({ message: 'Failed giving comments' });
+    }
+  },
 };
 
 export default commentsController;
