5834f016d20c84202c49cb7286067dc7e98b23ae
alsrlqor <skyup9393@gmail.com>
2022-09-08 21:25:34 +0900

    #82 Use env for SMTP

--- server/config/config.ts
+++ server/config/config.ts
@@ -2,19 +2,21 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 dotenv.config({
-    path: path.resolve(
-        process.env.NODE_ENV === 'production'
-            ? '.prod.env' : process.env.NODE_ENV === 'test'
-                ? '.test.env' : '.dev.env'
-    )
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.prod.env'
+      : process.env.NODE_ENV === 'test'
+      ? '.test.env'
+      : '.dev.env',
+  ),
 });
 
 export const config = {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    dialect: 'mysql',
-    timezone: '+09:00'
-}
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: 'mysql',
+  timezone: '+09:00',
+};
