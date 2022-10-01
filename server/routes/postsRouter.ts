8a42efae64beb0c492bd01cbebece2930fd2a02e
mingi <skyup9393@gmail.com>
2022-10-01 20:26:27 +0900

    Fix api order

--- server/routes/postsRouter.ts
+++ server/routes/postsRouter.ts
@@ -3,9 +3,9 @@ import PostsController from '../controllers/postsController';
 
 const router = Router();
 
+router.get('/', PostsController.getPost);
 router.post('/', PostsController.createPost);
 router.patch('/', PostsController.modifyPost);
 router.delete('/', PostsController.deletePost);
-router.get('/', PostsController.getPost);
 
 export default router;
