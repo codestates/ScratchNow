8a533e109996467d815174a61297df8fe7f04de4
mingi <skyup9393@gmail.com>
2022-10-01 20:27:15 +0900

    Add orm option and use request query

--- server/controllers/postsController.ts
+++ server/controllers/postsController.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express';
 import { Post } from '../models/post';
+import { User } from '../models/user';
 import { Comment } from '../models/comment';
 import { Liking } from '../models/liking';
 import { tokenAuthentication } from './authFunctions';
 
 const postsController = {
   getPost: async (req: Request, res: Response) => {
-    const { id } = req.body;
+    const { id } = req.query;
 
     try {
-      await Post.findOne({ where: { id } }).then((data) => {
+      await Post.findOne({
+        where: { id: Number(id) },
+        include: [
+          {
+            model: User,
+            as: 'userHasManyPosts',
+            attributes: ['id', 'nickname', 'profile_image_url'],
+          },
+        ],
+      }).then((data) => {
         res.json({ data: data, message: `Post detail of post id ${id}` });
       });
     } catch (err) {
@@ -27,17 +37,18 @@ const postsController = {
 
   modifyPost: async (req: Request, res: Response) => {
     const tokenValidity = tokenAuthentication(req);
-    const { id, painting_url, text } = req.body;
+    const { post_id, painting_url, text } = req.body;
 
     if (!tokenValidity) {
       res.status(404).json({ message: 'Invalid Token' });
     } else {
       try {
-        await Post.update({ painting_url, text }, { where: { id } }).then(
-          () => {
-            res.status(200).json({ message: `Modified the post` });
-          },
-        );
+        await Post.update(
+          { painting_url, text },
+          { where: { id: post_id } },
+        ).then(() => {
+          res.status(200).json({ message: `Modified the post ${post_id}` });
+        });
       } catch (err) {
         res.status(404).json({ message: 'Invalid post id' });
       }
@@ -46,21 +57,19 @@ const postsController = {
 
   deletePost: async (req: Request, res: Response) => {
     const tokenValidity = tokenAuthentication(req);
-    const { id } = req.body;
+    const { id } = req.query;
 
     if (!tokenValidity) {
       res.status(404).json({ message: 'Invalid Token' });
     } else {
-      try {
-        await Post.destroy({ where: { id } }).then(() => {
-          Comment.destroy({ where: { post_id: id } });
-          Liking.destroy({ where: { post_id: id } });
-        }).then(() => {
-          res.status(200).json({ message: `Soft deleted the post` });
+      await Post.destroy({ where: { id: Number(id) } })
+        .then(() => {
+          Comment.destroy({ where: { post_id: Number(id) } });
+          Liking.destroy({ where: { post_id: Number(id) } });
+        })
+        .then(() => {
+          res.status(200).json({ message: `Soft deleted the post ${id}` });
         });
-      } catch (err) {
-        res.status(404).json({ message: 'Invalid post id' });
-      }
     }
   },
 };
